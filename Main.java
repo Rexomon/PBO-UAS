377823badfed2b9f39b5141404e08e8803775802
Rexomon <muhammadindriyo12@gmail.com>
2024-12-19 19:54:07 +0700

    Main and StokPakan

--- Main.java
+++ Main.java
@@ -1,9 +1,69 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Pekerja pekerja = new Pekerja(1, "nama", "tugas", "kontak");
+        StokPakan stokPakan = new StokPakan("JenisPakan", 10, new java.util.Date());
 
+        int pilihan;
 
-        Sapi sapi = new Sapi("Mahmud", "Sapi", 3);
+        do {
+            System.out.println("Selamat datang di Aplikasi Manajemen Hewan");
+            System.out.println("1. Tambah Hewan");
+            System.out.println("2. Data Hewan");
+            System.out.println("3, Ubah Data Hewan");
+            System.out.println("4. Tambah Pekerja");
+            System.out.println("5. Tambah Stok Pakan");
+            System.out.println("6. Data Pakan");
+            System.out.println("7. Laporan Harian Pekerja");
+            System.out.println("8. Laporan Produksi");
+            System.out.println("9. Keluar");
+            System.out.print("Masukkan pilihan anda : ");
 
-        sapi.dataHewan();
+            pilihan = input.nextInt();
+            input.nextLine();
+
+            if (pilihan == 1) {
+                System.out.println("====Tambah Hewan====");
+
+            }
+            if (pilihan == 2) {
+                System.out.println("====Data Hewan====");
+
+            }
+
+            if (pilihan == 3) {
+                System.out.println("====Ubah Data Hewan====");
+
+            }
+
+            if (pilihan == 4) {
+                System.out.println("\n====Tambah Pekerja====");
+                pekerja.tambahData();
+            }
+
+            if (pilihan == 5) {
+                System.out.println("====Tambah Stok Pakan====");
+                stokPakan.tambahData();
+            }
+
+            if (pilihan == 6) {
+                System.out.println("====Data Pakan====");
+                stokPakan.laporanStokPakan();
+            }
+
+            if (pilihan == 7) {
+                System.out.println("\n====Laporan Pekerja====");
+                pekerja.laporanTugasHarian();
+            }
+            if (pilihan == 8) {
+                System.out.println("Pilihan 5 dipilih");
+
+            }
+
+        } while (pilihan != 8);
+
+        input.close();
     }
 }
--- StokPakan.java
+++ StokPakan.java
@@ -1,4 +1,13 @@
-public class StokPakan implements Manajemen{
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Scanner;
+
+public class StokPakan implements Manajemen {
+    Scanner input = new Scanner(System.in);
+
+    private ArrayList<StokPakan> stokPakan = new ArrayList<StokPakan>();
     private String JenisPakan;
     private int JumlahPakan;
     private Date TanggalKadaluarsa;
@@ -7,6 +16,42 @@ public class StokPakan implements Manajemen{
         this.JenisPakan = JenisPakan;
         this.JumlahPakan = JumlahPakan;
         this.TanggalKadaluarsa = TanggalKadaluarsa;
+    }
+
+    @Override
+    public void hapusData() {
+
+    }
+
+    @Override
+    public void perbaruiData() {
+        System.out.println("data berhasil di perbarui");
+    }
+
+    @Override
+    public void tambahData() {
+        System.out.print("Masukkan jenis pakan : ");
+        JenisPakan = input.nextLine();
+        System.out.print("Masukkan jumlah pakan : ");
+        JumlahPakan = input.nextInt();
+        System.out.print("Masukkan tanggal kadaluarsa (dd-MM-yyyy) : ");
+        String tanggal = input.next();
+        try {
+            TanggalKadaluarsa = new SimpleDateFormat("dd-MM-yyyy").parse(tanggal);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        System.out.print("\n");
+
+        stokPakan.add(new StokPakan(JenisPakan, JumlahPakan, TanggalKadaluarsa));
+    }
+
+    public void laporanStokPakan() {
+        for (StokPakan stok : stokPakan) {
+            System.out.println("\nJenis Pakan : " + stok.getJenisPakan());
+            System.out.println("Jumlah Pakan : " + stok.getJumlahPakan());
+            System.out.println("Tanggal Kadaluarsa : " + stok.getTanggalKadaluasa() + "\n");
+        }
     }
 
     public String getJenisPakan() {
@@ -21,19 +66,4 @@ public class StokPakan implements Manajemen{
         return TanggalKadaluarsa;
     }
 
-    @Override
-    public void hapusData() {
-        
-    }
-
-    @Override
-    public void perbaruiData() {
-        System.out.println("data berhasil di perbarui");
-    }
-
-    @Override
-    public void tambahData() {
-        System.out.println("data berhasil di tambahkan");
-        
-    }
 }
